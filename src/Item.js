23370b5f98009e55eb45163f823ed60c80474a33
FOLI <you@example.com>
2022-02-05 21:43:59 +0330

    add disability and option arrow SVG

--- src/Item.js
+++ src/Item.js
@@ -1,20 +1,19 @@
+import RightArrow from "./RightArrow";
+
 function Item(props) {
+  // let rightArrow = <RightArrow></RightArrow>;
     return (
         <li className="item">
-            <button onClick={props.onClick}>
+            <button onClick={props.onClick} disabled={props.isDisabled}>
                 <div className="item__icon-wrapper">
-                    {props.hasIcon ? (
-                        props.Icon
-                    ) : undefined}
+                    {props.hasIcon ? props.Icon : null}
                 </div>
                 <p className="item__text">{props.children} </p>
                 <small className="item__shoutcut">{props.shortCut}</small>
                 <div className="item__option-wrapper">
-                    {props.hasOptions ? (
-                        <img src={props.option} alt="options" />
-                    ) : undefined}
+                    {props.hasOptions ? <RightArrow />: null}
                 </div>
-        </button>
+            </button>
         </li>
     );
 }
