3fd522fe085e6ae73e70ec05bf61fb10449ffe5f
FOLI <you@example.com>
2022-02-05 21:44:44 +0330

    add Index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import OptionList from "./OptionList";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <article>
+            <OptionList></OptionList>
+        </article>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
