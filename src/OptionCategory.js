15705f607642cddf85a40f3433cf751b9d7a20b5
FOLI <you@example.com>
2022-02-06 22:16:39 +0330

    fix bad class names

--- src/OptionCategory.js
+++ src/OptionCategory.js
@@ -1,11 +1,7 @@
-import Item from "./Item.js"
+import Item from "./Item.js";
 
-const OptionCategory = ({children}) => {
-    return (
-        <ul className="optionCategory">
-            {children}
-        </ul>
-     );
-}
+const OptionCategory = ({ children }) => {
+    return <ul className="option-category">{children}</ul>;
+};
 
 export default OptionCategory;
