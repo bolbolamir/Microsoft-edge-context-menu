884afe12ed7d9506b09d355dcf6b37f1488e15f5
FOLI <you@example.com>
2022-02-06 22:16:27 +0330

    fix excessive article

--- src/OptionList.js
+++ src/OptionList.js
@@ -89,73 +89,67 @@ const OptionList = ({}) => {
     );
     return (
         <>
-            <article className="optionList">
-                <ul className="optionList__list">
-                    <OptionCategory>
-                        <Item
-                            hasIcon={true}
-                            Icon={SVG_24hour}
-                            shortCut="Alt+Left arrow"
-                        >
-                            Back
-                        </Item>
-                        <Item
-                            hasIcon={true}
-                            Icon={SVG_Add}
-                            shortCut="Alt+Right arrow"
-                            isDisabled={true}
-                        >
-                            Forward
-                        </Item>
-                        <Item shortCut="Ctrl+R">Refresh</Item>
-                    </OptionCategory>
-                    <OptionCategory>
-                        <Item
-                            hasIcon={true}
-                            Icon={SVG_ReadAloud}
-                            shortCut="Ctrl+S"
-                        >
-                            Save as
-                        </Item>
-                        <Item shortCut="Ctrl+P">Print</Item>
-                        <Item hasIcon={true} Icon={SVG_Notify}>
-                            Cast media to device
-                        </Item>
-                    </OptionCategory>
-                    <OptionCategory>
-                        <Item
-                            hasIcon={true}
-                            Icon={SVG_Gallery}
-                            shortCut="Ctrl+Shift+U"
-                        >
-                            Read aloud
-                        </Item>
-                        <Item hasIcon={true} Icon={SVG_ReadAloud}>
-                            Translate to English
-                        </Item>
-                    </OptionCategory>
-                    <OptionCategory>
-                        <Item
-                            hasOptions={true}
-                            options={
-                                <OptionCategory data-ispopover={true}>
-                                    <Item hasIcon={true} Icon={SVG_ReadAloud}>
-                                        uuu
-                                    </Item>
-                                    <Item hasIcon={true} Icon={SVG_ReadAloud}>
-                                        uuu
-                                    </Item>
-                                    <Item hasIcon={true} Icon={SVG_ReadAloud}>
-                                        uuu
-                                    </Item>
-                                </OptionCategory>
-                            }
-                        >
-                            ffwe
-                        </Item>
-                    </OptionCategory>
-                </ul>
-            </article>
+            <ul className="option-list">
+                <OptionCategory>
+                    <Item
+                        hasIcon={true}
+                        Icon={SVG_24hour}
+                        shortCut="Alt+Left arrow"
+                    >
+                        Back
+                    </Item>
+                    <Item
+                        hasIcon={true}
+                        Icon={SVG_Add}
+                        shortCut="Alt+Right arrow"
+                        isDisabled={true}
+                    >
+                        Forward
+                    </Item>
+                    <Item shortCut="Ctrl+R">Refresh</Item>
+                </OptionCategory>
+                <OptionCategory>
+                    <Item hasIcon={true} Icon={SVG_ReadAloud} shortCut="Ctrl+S">
+                        Save as
+                    </Item>
+                    <Item shortCut="Ctrl+P">Print</Item>
+                    <Item hasIcon={true} Icon={SVG_Notify}>
+                        Cast media to device
+                    </Item>
+                </OptionCategory>
+                <OptionCategory>
+                    <Item
+                        hasIcon={true}
+                        Icon={SVG_Gallery}
+                        shortCut="Ctrl+Shift+U"
+                    >
+                        Read aloud
+                    </Item>
+                    <Item hasIcon={true} Icon={SVG_ReadAloud}>
+                        Translate to English
+                    </Item>
+                </OptionCategory>
+                <OptionCategory>
+                    <Item
+                        hasOptions={true}
+                        options={
+                            <OptionCategory data-ispopover={true}>
+                                <Item hasIcon={true} Icon={SVG_ReadAloud}>
+                                    uuu
+                                </Item>
+                                <Item hasIcon={true} Icon={SVG_ReadAloud}>
+                                    uuu
+                                </Item>
+                                <Item hasIcon={true} Icon={SVG_ReadAloud}>
+                                    uuu
+                                </Item>
+                            </OptionCategory>
+                        }
+                    >
+                        ffwe
+                    </Item>
+                </OptionCategory>
+            </ul>
         </>
     );
 };
