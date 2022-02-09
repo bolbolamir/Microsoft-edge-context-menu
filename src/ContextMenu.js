800651f093f27b08849976ac329217c85560ff30
FOLI <you@example.com>
2022-02-09 11:22:21 +0330

    Remove exccesive fragments

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -3,69 +3,63 @@ import { Item } from "./Item.js";
 
 const ContextMenu = ({}) => {
     return (
-        <>
-            <ul className="context-menu">
-                <ItemsCategory>
-                    <Item
-                        hasIcon={true}
-                        icon={SVG_24hour}
-                        shortCut="Alt+Left arrow"
-                    >
-                        Back
-                    </Item>
-                    <Item
-                        hasIcon={true}
-                        icon={SVG_Add}
-                        shortCut="Alt+Right arrow"
-                        isDisabled={true}
-                    >
-                        Forward
-                    </Item>
-                    <Item shortCut="Ctrl+R">Refresh</Item>
-                </ItemsCategory>
-                <ItemsCategory>
-                    <Item hasIcon={true} icon={SVG_ReadAloud} shortCut="Ctrl+S">
-                        Save as
-                    </Item>
-                    <Item shortCut="Ctrl+P">Print</Item>
-                    <Item hasIcon={true} icon={SVG_Notify}>
-                        Cast media to device
-                    </Item>
-                </ItemsCategory>
-                <ItemsCategory>
-                    <Item
-                        hasIcon={true}
-                        icon={SVG_Gallery}
-                        shortCut="Ctrl+Shift+U"
-                    >
-                        Read aloud
-                    </Item>
-                    <Item hasIcon={true} icon={SVG_ReadAloud}>
-                        Translate to English
-                    </Item>
-                </ItemsCategory>
-                <ItemsCategory>
-                    <Item
-                        hasOptions={true}
-                        options={
-                            <ItemsCategory isPopOver={true}>
-                                <Item hasIcon={true} icon={SVG_ReadAloud}>
-                                    uuu
-                                </Item>
-                                <Item hasIcon={true} icon={SVG_ReadAloud}>
-                                    uuu
-                                </Item>
-                                <Item hasIcon={true} icon={SVG_ReadAloud}>
-                                    uuu
-                                </Item>
-                            </ItemsCategory>
-                        }
-                    >
-                        ffwe
-                    </Item>
-                </ItemsCategory>
-            </ul>
-        </>
+        <ul className="context-menu">
+            <ItemsCategory>
+                <Item
+                    hasIcon={true}
+                    icon={SVG_24hour}
+                    shortCut="Alt+Left arrow"
+                >
+                    Back
+                </Item>
+                <Item
+                    hasIcon={true}
+                    icon={SVG_Add}
+                    shortCut="Alt+Right arrow"
+                    isDisabled={true}
+                >
+                    Forward
+                </Item>
+                <Item shortCut="Ctrl+R">Refresh</Item>
+            </ItemsCategory>
+            <ItemsCategory>
+                <Item hasIcon={true} icon={SVG_ReadAloud} shortCut="Ctrl+S">
+                    Save as
+                </Item>
+                <Item shortCut="Ctrl+P">Print</Item>
+                <Item hasIcon={true} icon={SVG_Notify}>
+                    Cast media to device
+                </Item>
+            </ItemsCategory>
+            <ItemsCategory>
+                <Item hasIcon={true} icon={SVG_Gallery} shortCut="Ctrl+Shift+U">
+                    Read aloud
+                </Item>
+                <Item hasIcon={true} icon={SVG_ReadAloud}>
+                    Translate to English
+                </Item>
+            </ItemsCategory>
+            <ItemsCategory>
+                <Item
+                    hasOptions={true}
+                    options={
+                        <ItemsCategory isPopOver={true}>
+                            <Item hasIcon={true} icon={SVG_ReadAloud}>
+                                uuu
+                            </Item>
+                            <Item hasIcon={true} icon={SVG_ReadAloud}>
+                                uuu
+                            </Item>
+                            <Item hasIcon={true} icon={SVG_ReadAloud}>
+                                uuu
+                            </Item>
+                        </ItemsCategory>
+                    }
+                >
+                    ffwe
+                </Item>
+            </ItemsCategory>
+        </ul>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { ContextMenu } from "./ContextMenu.js";
 
 ReactDOM.render(
     <React.StrictMode>
-        <>
-            <ContextMenu></ContextMenu>
-        </>
+        <ContextMenu></ContextMenu>
     </React.StrictMode>,
     document.getElementById("root")
 );
