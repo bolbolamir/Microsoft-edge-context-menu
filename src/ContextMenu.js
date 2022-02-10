6ac32872c16e31e65a1be7c9c191637e23ed4a58
FOLI <you@example.com>
2022-02-10 12:00:41 +0330

    merge item category into menu

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -3,7 +3,7 @@
 const ContextMenu = ({}) => {
     return (
         <ul className="context-menu">
-            <ItemsCategory>
+            <>
                 <Item
                     hasIcon={true}
                     icon={SVG_24hour}
@@ -20,8 +20,8 @@ const ContextMenu = ({}) => {
                     Forward
                 </Item>
                 <Item shortCut="Ctrl+R">Refresh</Item>
-            </ItemsCategory>
-            <ItemsCategory>
+            </>
+            <>
                 <Item hasIcon={true} icon={SVG_ReadAloud} shortCut="Ctrl+S">
                     Save as
                 </Item>
@@ -29,20 +29,20 @@ const ContextMenu = ({}) => {
                 <Item hasIcon={true} icon={SVG_Notify}>
                     Cast media to device
                 </Item>
-            </ItemsCategory>
-            <ItemsCategory>
+            </>
+            <>
                 <Item hasIcon={true} icon={SVG_Gallery} shortCut="Ctrl+Shift+U">
                     Read aloud
                 </Item>
                 <Item hasIcon={true} icon={SVG_ReadAloud}>
                     Translate to English
                 </Item>
-            </ItemsCategory>
-            <ItemsCategory>
+            </>
+            <>
                 <Item
                     hasOptions={true}
                     options={
-                        <ItemsCategory isPopOver={true}>
+                        <div isPopOver={true} data-isPopOver>
                             <Item hasIcon={true} icon={SVG_ReadAloud}>
                                 uuu
                             </Item>
@@ -52,67 +52,70 @@ const ContextMenu = ({}) => {
                             <Item hasIcon={true} icon={SVG_ReadAloud}>
                                 uuu
                             </Item>
-                        </ItemsCategory>
+                        </div>
                     }
                 >
                     ffwe
                 </Item>
-            </ItemsCategory>
+            </>
         </ul>
     );
 };
 
 
-export { ContextMenu };
-
-    const ItemsCategory = ({ children, isPopOver }) => {
-        return (
-            <li
-                className="items-category"
-                data-ispopover={isPopOver}
-                aria-haspopup={isPopOver ? "menu" : null}
-            >
-                <ul>{children}</ul>
-            </li>
-        );
-    };
 
 
-    const Item = ({
-        onClick,
-        isDisabled,
-        children,
-        options,
-        icon,
-        shortCut,
-    }) => {
-        return (
-            <li className="item">
-                <article onClick={onClick} disabled={isDisabled} role="button">
-                    <div className="item-icon-wrapper" aria-hidden="true">
+
+const Item = ({
+    onClick,
+    isDisabled,
+    children,
+    options,
+    icon,
+    shortCut,
+}) => {
+    return (
+        <li className="menu-item">
+                <button onClick={onClick} disabled={isDisabled}>
+                    <span className="item-icon-wrapper" aria-hidden="true">
                         {icon}
-                    </div>
-                    <p className="item-text">{children}</p>
-
-                    <small className="item-shoutcut" aria-hidden={!shortCut}>
-                        <kbd aria-label="key binding">{shortCut}</kbd>
-                    </small>
-                    <div className="item-option-wrapper" aria-hidden="true">
+                    </span>
+                    <span className="item-text">{children}</span>
+                        <kbd
+                            aria-label="key binding"
+                            className="item-shoutcut"
+                            aria-hidden={!shortCut}
+                            >
+                            {shortCut}
+                        </kbd>
+                    <span className="item-option-wrapper" aria-hidden="true">
                         {options ? <SVG_RightArrow /> : null}
-                    </div>
+                    </span>
                     {options}
-                </article>
+                </button>
             </li>
         );
     };
 
 
-const SVG_RightArrow = ({}) => (
-    <svg
-        
+        const ItemsCategory = ({ children, isPopOver }) => {
+            return (
+                <li
+                    className="items-category"
+                    data-ispopover={isPopOver}
+                    aria-haspopup={isPopOver ? "menu" : null}
+                >
+                    <ul>{children}</ul>
+                </li>
+            );
+        };
+
+    const SVG_RightArrow = ({}) => (
+        <svg
+
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1024 1024"
-    >
+        >
         <path
             d="M332.8 854.016c-17.408-17.408-18.944-36.352-11.264-44.032L619.52 512 321.536 214.016c-15.36-15.36-14.848-29.184 5.632-49.664s36.864-18.432 49.664-5.632l330.24 330.24c14.336 14.336 13.824 29.696 0.512 45.056L376.832 865.28c-7.168 7.168-26.624 6.144-44.032-11.264z"
             fill="#2C2C2C"
@@ -194,3 +197,4 @@ let SVG_Notify = (
         />
     </svg>
 );
+export { ContextMenu };
