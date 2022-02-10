364dce58d595ea918da229f960a2de0324da6acb
FOLI <you@example.com>
2022-02-10 12:16:39 +0330

    add key item to data model

--- src/index.js
+++ src/index.js
@@ -11,37 +11,44 @@ ReactDOM.render(
                     name: "Back",
                     shortcut: "Alt+Left arrow",
                     icon: "SVG_24hour",
+                    id: 1,
                 },
                 {
                     name: "Forward",
                     shortcut: "Alt+Right arrow",
                     icon: "SVG_Add",
                     isDisabled: true,
+                    id: 2,
                 },
                 {
                     name: ">Refresh",
                     shortcut: "Ctrl+R",
+                    id: 3,
                 },
                 {
                     name: "Save as",
                     shortcut: "Ctrl+S",
                     icon: "SVG_ReadAloud",
+                    id: 4,
                 },
                 {
                     name: "Cast media to device",
                     icon: "SVG_Notify",
+                    id: 5,
                 },
                 {
                     name: "See more",
+                    id: 6,
                     options: [
-                        { name: "Back", shortcut: "Alt+Left arrow" },
+                        { name: "Back", shortcut: "Alt+Left arrow", id: 6.1 },
                         {
                             name: "Forward",
                             shortcut: "Alt+Right arrow",
                             icon: "SVG_Add",
                             isDisabled: true,
+                            id: 6.2,
                         },
-                        { name: "xxz" },
+                        { name: "xxz", id: 6.3 },
                     ],
                 },
             ]}
