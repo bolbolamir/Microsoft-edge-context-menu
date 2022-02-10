e5df10e889f528d55bef9e2583e1ebdd21dc00a5
FOLI <you@example.com>
2022-02-10 12:13:07 +0330

    add data model for mapping

--- src/index.js
+++ src/index.js
@@ -5,7 +5,47 @@ import { ContextMenu } from "./ContextMenu.js";
 
 ReactDOM.render(
     <React.StrictMode>
-        <ContextMenu items={0}></ContextMenu>
+        <ContextMenu
+            items={[
+                {
+                    name: "Back",
+                    shortcut: "Alt+Left arrow",
+                    icon: "SVG_24hour",
+                },
+                {
+                    name: "Forward",
+                    shortcut: "Alt+Right arrow",
+                    icon: "SVG_Add",
+                    isDisabled: true,
+                },
+                {
+                    name: ">Refresh",
+                    shortcut: "Ctrl+R",
+                },
+                {
+                    name: "Save as",
+                    shortcut: "Ctrl+S",
+                    icon: "SVG_ReadAloud",
+                },
+                {
+                    name: "Cast media to device",
+                    icon: "SVG_Notify",
+                },
+                {
+                    name: "See more",
+                    options: [
+                        { name: "Back", shortcut: "Alt+Left arrow" },
+                        {
+                            name: "Forward",
+                            shortcut: "Alt+Right arrow",
+                            icon: "SVG_Add",
+                            isDisabled: true,
+                        },
+                        { name: "xxz" },
+                    ],
+                },
+            ]}
+        ></ContextMenu>
     </React.StrictMode>,
     document.getElementById("root")
 );
