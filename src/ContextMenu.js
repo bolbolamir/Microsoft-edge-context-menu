765cb7eda7d3ee2ae01cef5108f2fcb59ceae12d
FOLI <you@example.com>
2022-02-11 21:59:03 +0330

    add nesting level

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,6 +1,6 @@
 import { icons } from "./icons";
 
-function itemify(item) {
+function itemify(item , nestingLevel = 0) {
     return !item.isSeperator ? (
         <li className="menu-item" key={item.id.toString()}>
             <button onClick={item.onClick} disabled={item.isDisabled}>
@@ -14,9 +14,9 @@ function itemify(item) {
                 </span>
             </button>
             {item.options ? (
-                <ul data-ispopover="true" className="context-menu" role="menu">
+                <ul data-ispopover="true" className="context-menu" role="menu" data-nestingLevel = {nestingLevel}>
                     {item.options.map((item) => {
-                        return itemify(item);
+                        return itemify(item, nestingLevel + 1);
                     })}
                 </ul>
             ) : null}
