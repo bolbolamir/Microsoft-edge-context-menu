8d88ba447087f91ab6a66c8953c49832141c04ca
FOLI <you@example.com>
2022-02-11 21:47:49 +0330

    finalize refactoring

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,70 +1,34 @@
 import { icons } from "./icons";
 
 function itemify(item) {
-    console.log(item);
-    return (
+    return !item.isSeperator ? (
         <li className="menu-item" key={item.id.toString()}>
             <button onClick={item.onClick} disabled={item.isDisabled}>
-                <span className="item-icon-wrapper" aria-hidden="true">
-                    {item.icon}
-                </span>
-                <span className="item-text">{item.name}</span>
-                <kbd
-                    aria-label="key binding"
-                    className="item-shoutcut"
-                    aria-hidden={!item.shortcut}
-                >
+                <span aria-hidden="true">{item.icon}</span>
+                {item.name}
+                <kbd aria-label="key binding" aria-hidden={!item.shortcut}>
                     {item.shortcut}
                 </kbd>
-                <span className="item-option-wrapper" aria-hidden="true">
+                <span aria-hidden="true">
                     {item.options != null ? icons.SVG_RightArrow : null}
                 </span>
             </button>
-                {item.options ? (
-                <ul data-ispopover="true">{item.options.map((item)=>{return itemify(item)})}</ul>
-                ) : null}
+            {item.options ? (
+                <ul data-ispopover="true" className="context-menu">
+                    {item.options.map((item) => {
+                        return itemify(item);
+                    })}
+                </ul>
+            ) : null}
         </li>
+    ) : (
+        <li role="separator" key={item.id.toString()}></li>
     );
 }
 
 const ContextMenu = ({ items }) => {
-    let mappedItems = items.map((item) => { return itemify(item) });
+    let mappedItems = items.map((item) => itemify(item));
     return <ul className="context-menu">{mappedItems}</ul>;
 };
 
-// const Item = ({ onClick, isDisabled, children, options, icon, shortCut }) => {
-//     return (
-//         <li className="menu-item">
-//             <button onClick={onClick} disabled={isDisabled}>
-//                 <span className="item-icon-wrapper" aria-hidden="true">
-//                     {icon}
-//                 </span>
-//                 <span className="item-text">{children}</span>
-//                 <kbd
-//                     aria-label="key binding"
-//                     className="item-shoutcut"
-//                     aria-hidden={!shortCut}
-//                 >
-//                     {shortCut}
-//                 </kbd>
-//                 <span className="item-option-wrapper" aria-hidden="true">
-//                     {options ? <SVG_RightArrow /> : null}
-//                 </span>
-//                 {options}
-//             </button>
-//         </li>
-//     );
-// };
-
-// const ItemsCategory = ({ children, isPopOver }) => {
-//     return (
-//         <li
-//             className="items-category"
-//             data-ispopover={isPopOver}
-//             aria-haspopup={isPopOver ? "menu" : null}
-//         >
-//             <ul>{children}</ul>
-//         </li>
-//     );
-// };
 export { ContextMenu };
